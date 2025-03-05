eb7554a78f0e43c9fabe27d3da5532166ea4ab36
Nekit <nick.misiura@yandex.ru>
2025-03-05 12:39:59 +0300

    feat: make item a grid with span for 1 column

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -23,11 +23,13 @@ import "./Item.css";
 export default function Item({onDeleteItem, onMoveUpButton, onMoveDownButton, id, children}) {
 
     return (
-        <li className="item">
-            {children}
-            <button className="move-item" onClick={() => onMoveUpButton(id)}>↑</button>
-            <button className="move-item" onClick={() => onMoveDownButton(id)}>↓</button>
-            <button onClick={() => onDeleteItem(id)} className="delete-item">X</button>
-        </li>
+        <div className="item">
+            <li>{children}</li>
+            <div className="item-control">
+                <button className="move-item" onClick={() => onMoveUpButton(id)}>↑</button>
+                <button className="move-item" onClick={() => onMoveDownButton(id)}>↓</button>
+                <button onClick={() => onDeleteItem(id)} className="delete-item">X</button>
+            </div>
+        </div>
     );
 }
