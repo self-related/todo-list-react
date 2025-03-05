09dfeb497f3f9ead87f9783ea93313556ecee7d2
Nekit <nick.misiura@yandex.ru>
2025-03-05 10:31:22 +0300

    feat: handle add new item; handle change text input

--- src/components/ToDoApp.jsx
+++ src/components/ToDoApp.jsx
@@ -1,17 +1,34 @@
+import { useState } from "react";
 import "./ToDoApp.css";
 
 export default function ToDoApp() {
 
+    const [items, setItems] = useState(["test1"]);
+    const [newItem, setNewItem] = useState("");
     
+    const handleInputEdit = (event) => {
+        /**@type {HTMLInputElement} */
+        const inputElement = event.target;
+        
+        setNewItem(inputElement.value);
+    };
+
+    const handleAddButtonClick = () => {
+        setItems([...items, newItem]);
+    };
+
+
     return (
         <div id="todo-app">
             <h1>ToDo List</h1>
             <div class="app-panel">
-                <input type="text" />
-                <button>Add</button>
+                <input type="text" onChange={handleInputEdit} value={newItem} />
+                <button onClick={handleAddButtonClick}>Add</button>
             </div>
             <div class="list">
-
+                <ol>
+                    {items}
+                </ol>
             </div>
         </div>
     );
