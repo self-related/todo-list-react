7c15b8f22ecb59abb9bd503e18948ef5d6dccc9c
Nekit <nick.misiura@yandex.ru>
2025-03-05 12:43:02 +0300

    ref: remove navbar and footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,13 @@
 import './App.css'
-import Footer from './components/Footer'
-import Navbar from './components/Navbar'
 import ToDoApp from './components/ToDoApp'
 
 function App() {
 
   return (
     <>
-        <Navbar />
-        
         <main>
             <ToDoApp />
         </main>
-
-        <Footer />
     </>
   )
 }
