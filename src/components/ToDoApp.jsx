1baec869a1c0aec4661c89e6d2c5b9cffd5cae71
Nekit <nick.misiura@yandex.ru>
2025-03-05 09:08:45 +0300

    add input panel; styling for ToDoApp component

--- src/components/ToDoApp.jsx
+++ src/components/ToDoApp.jsx
@@ -1,9 +1,18 @@
-
+import "./ToDoApp.css";
 
 export default function ToDoApp() {
 
     
     return (
-        <div id="todo-app">ToDo App</div>
+        <div id="todo-app">
+            <h1>ToDo List</h1>
+            <div class="app-panel">
+                <input type="text" />
+                <button>Add</button>
+            </div>
+            <div class="list">
+
+            </div>
+        </div>
     );
 }
